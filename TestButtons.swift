6917f0cc626755815aec451eb8ce81f01eae53ee
FernandoHazel <fernandohaze1@gmail.com>
2024-02-13 16:08:55 -0600

    Added Crashlytics

--- TestButtons.swift
+++ TestButtons.swift
@@ -1,15 +1,49 @@
-//
-//  TestButtons.swift
-//  Reyes
-//
-//  Created by Fernando Ascencio on 13/02/24.
-//
-
 import SwiftUI
+import FirebaseCore
+import FirebaseAnalytics
+
 
 struct TestButtons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            Button(action: {
+                //Trigger analytic event
+                Analytics.logEvent("Test_Button_pressed", parameters: [
+                    AnalyticsParameterItemID: "Test_button",
+                    AnalyticsParameterItemName: "Reyes_Button",
+                ])
+            }, label: {
+                Text("Analytic")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    )
+            })
+            Button(action: {
+                //Trigger error
+                fatalError("Crash was triggered")
+            }, label: {
+                Text("Error")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal, 30)
+                    .background(
+                        Color.red
+                            .cornerRadius(10)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    )
+            })
+        }
+        
     }
 }
 
