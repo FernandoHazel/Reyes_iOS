c117df57263178089ada285fd7f39d229a872775
FernandoHazel <fernandohaze1@gmail.com>
2024-02-13 16:08:28 -0600

    Added Crashlytics

---
+++ TestButtons.swift
@@ -0,0 +1,18 @@
+//
+//  TestButtons.swift
+//  Reyes
+//
+//  Created by Fernando Ascencio on 13/02/24.
+//
+
+import SwiftUI
+
+struct TestButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestButtons()
+}
