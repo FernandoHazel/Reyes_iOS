8cafa326970faa506f36fcad2a84308ba3db54d7
FernandoHazel <fernandohaze1@gmail.com>
2024-02-12 20:05:25 -0600

    Initial Commit

---
+++ Reyes/ReyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReyesApp.swift
+//  Reyes
+//
+//  Created by Fernando Ascencio on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ReyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
