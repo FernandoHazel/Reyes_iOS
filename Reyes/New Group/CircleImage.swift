9976f9a6282dadec0a5d567aa9267bae8148d3ac
FernandoHazel <fernandohaze1@gmail.com>
2024-02-16 16:49:04 -0600

    detail view added

---
+++ Reyes/New Group/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Reyes
+//
+//  Created by Fernando Ascencio on 16/02/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
