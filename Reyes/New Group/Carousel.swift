fd3a947354ecb05e1d4d7f40eb784044a0947dbe
FernandoHazel <fernandohaze1@gmail.com>
2024-02-25 13:34:44 -0600

    Improved shop styles

---
+++ Reyes/New Group/Carousel.swift
@@ -0,0 +1,18 @@
+//
+//  Carousel.swift
+//  Reyes
+//
+//  Created by Fernando Ascencio on 25/02/24.
+//
+
+import SwiftUI
+
+struct Carousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Carousel()
+}
