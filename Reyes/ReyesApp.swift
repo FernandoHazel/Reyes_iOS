5efee5238a5063f2464e9cd44d0c699313a939de
Fernando Ascencio <fernando.ascencio@bradesco.com.mx>
2024-08-01 12:30:54 -0600

    Initial Commit

---
+++ Reyes/ReyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReyesApp.swift
+//  Reyes
+//
+//  Created by Fernando Hazel Ascencio Baumgarten on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ReyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
